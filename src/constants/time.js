fc7f80be68ac4fe907e217a58afc8fb91517be91
Hojun Lee <quantum0430@gmail.com>
2021-01-07 22:19:54 +0900

    time constants

--- src/constants/time.js
+++ src/constants/time.js
@@ -1,38 +1,8 @@
-import React, { Component } from 'react';
+export const OT_TIME = 20 * 60;
+export const ROUND1_VIDEO = 25 * 60;
+export const ROUND1_INVEST = 10 * 60;
+export const ROUND2_VIDEO = 25 * 60;
+export const ROUND2_INVEST = 10 * 60;
+export const ROUND3_VIDEO = 25 * 60;
+export const ROUND3_INVEST = 10 * 60;
 
-
-class Time extends Component {
-
-    constructor(props){
-        super(props);
-        this.state = {
-            START_TIME: 0,
-        }
-    }
-
-    date = 0;
-
-    componentDidMount(){
-        this.setState({
-            START_TIME: this.date,
-        });
-    }
-    setStartTime(){
-        this.setStartTimes(0);
-    }
-    setStartTimes(opt) {
-        console.log("hi");
-        if(opt===0){
-            console.log("hey");
-            this.date = Date.now();
-        }
-        else if(opt===1){
-            this.date = new Date(21,1,1,2,0,0).getTime();
-        }
-        
-        console.log(this.state.START_TIME);
-    }
-}
-const time = new Time();
-
-export default time;
