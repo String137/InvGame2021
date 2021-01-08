cd0be630843b88023d8b142a51537e83975c0350
SangminLee <sangminlee@SangminLeeui-MacBookPro.local>
2021-01-08 09:55:13 +0900

    getting money input added

--- src/components/InvPage/CompanyPage/index.js
+++ src/components/InvPage/CompanyPage/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Company from './Company';
 import AfterInv from './AfterInv';
 import CurInv from './CurInv';
 import Input from './Input';
 const CompanyPage = ({curm, name, aftm}) => {
+    const [inputm, setInputm] = useState(0);
+    const handleChange = (event) => {
+        console.log(event);
+        const {target: {value}} = event;
+        if(!isNaN(parseInt(value))){
+            setInputm(parseInt(value));
+        }
+        else{
+            setInputm(parseInt(prev=>prev));
+        }
+    }
     return(
         <div>
-            <Company name = {name} class = "company"/>
-            <CurInv money = {curm} class = "curinv"/>
-            <Input class="input"/>
+            <Company name = {name} className = "company"/>
+            <CurInv money = {curm} className = "curinv"/>
+            <input type="number" class="input" onChange={handleChange} value={inputm}/>
             <div>
                 <input type="radio" id={name+"inv"} name={name+"select"}/>
                 <label htmlFor={name+"inv"}>투자</label>
@@ -22,7 +33,7 @@ const CompanyPage = ({curm, name, aftm}) => {
                 <label htmlFor={name+"cancel"}>취소</label>
             </div>
             
-            <AfterInv money = {aftm}/>
+            <AfterInv money = {parseInt(curm)+ (inputm ? parseInt(inputm) :  0)}/>
         </div>
     );
 }
