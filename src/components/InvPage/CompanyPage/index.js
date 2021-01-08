aa6cbf46ecabb9f56137b85f74d65fee7a5760a5
SangminLee <sangminlee@SangminLeeui-MacBookPro.local>
2021-01-08 10:33:14 +0900

    radio button invest withdraw function added

--- src/components/InvPage/CompanyPage/index.js
+++ src/components/InvPage/CompanyPage/index.js
@@ -3,8 +3,10 @@ import Company from './Company';
 import AfterInv from './AfterInv';
 import CurInv from './CurInv';
 import Input from './Input';
-const CompanyPage = ({curm, name, aftm}) => {
+const CompanyPage = ({curm, name}) => {
+    var aftm = 0;
     const [inputm, setInputm] = useState(0);
+    const [radioNum, setRadioNum] = useState(0);
     const handleChange = (event) => {
         console.log(event);
         const {target: {value}} = event;
@@ -15,25 +17,35 @@ const CompanyPage = ({curm, name, aftm}) => {
             setInputm(parseInt(prev=>prev));
         }
     }
+    
+    switch (radioNum){
+        case 0:
+            aftm = parseInt(curm) + (inputm ? parseInt(inputm) :  0);
+            break;
+        case 1:
+            aftm = parseInt(curm) - (inputm ? parseInt(inputm) :  0);
+            break;
+        case 2:
+            aftm = parseInt(curm);
+            break;
+        default:
+            aftm = 0;
+    }
     return(
         <div>
             <Company name = {name} className = "company"/>
             <CurInv money = {curm} className = "curinv"/>
             <input type="number" class="input" onChange={handleChange} value={inputm}/>
             <div>
-                <input type="radio" id={name+"inv"} name={name+"select"}/>
+                <input type="radio" id={name+"inv"} name={name+"select"} onClick={()=>{setRadioNum(0)}}/>
                 <label htmlFor={name+"inv"}>투자</label>
             </div>
             <div>
-                <input type="radio" id={name+"wd"} name={name+"select"}/>
+                <input type="radio" id={name+"wd"} name={name+"select"} onClick={()=>{setRadioNum(1)}}/>
                 <label htmlFor={name+"wd"}>철회</label>
             </div>
-            <div>
-                <input type="radio" id={name+"cancel"} name={name+"select"}/>
-                <label htmlFor={name+"cancel"}>취소</label>
-            </div>
             
-            <AfterInv money = {parseInt(curm)+ (inputm ? parseInt(inputm) :  0)}/>
+            <AfterInv money = {aftm}/>
         </div>
     );
 }
