4b39ca328529a9f60ed931c9cfa9ece64b19ca50
Hojun Lee <quantum0430@gmail.com>
2021-01-25 22:09:52 +0900

    asdf

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -55,13 +55,13 @@ class AdminPage extends Component {
       loading: true,
     });
   }
-  componentDidUpdate(){
+  componentDidUpdate() {
     // if(this.rank1load===false){
     // this.getWinner().then(()=>{
     //   this.setState({rank1load: true});
     // });
   }
-  
+
   setequal1 = () => {
     var updates = {};
     updates['/equal1'] = true;
@@ -230,63 +230,63 @@ class AdminPage extends Component {
     else if (round === 1) {
       // console.log("hihhi");
       const snap = await this.props.firebase.db.ref('/companies/').once('value');
-      const list = snap.val().map(e=>({rank: e.round1rank, index: e.index, name: e.companyname}));
+      const list = snap.val().map(e => ({ rank: e.round1rank, index: e.index, name: e.companyname }));
       // console.log(list.map(e=>e));
-      list.sort(function(a, b){
+      list.sort(function (a, b) {
         if (a.rank > b.rank) {
           return 1;
         }
-        else{
+        else {
           return -1;
         }
       });
-      this.setState({curList: list});
-      this.setState({round: 1});
+      this.setState({ curList: list });
+      this.setState({ round: 1 });
       return list;
     }
     else if (round === 2) {
       const snap = await this.props.firebase.db.ref('/companies/').once('value');
-      const list = snap.val().map(e=>({rank: e.round2rank, index: e.index, name: e.companyname}));
-      list.sort(function(a, b){
+      const list = snap.val().map(e => ({ rank: e.round2rank, index: e.index, name: e.companyname }));
+      list.sort(function (a, b) {
         if (a.rank > b.rank) {
           return 1;
         }
-        else{
+        else {
           return -1;
         }
       });
-      this.setState({curList: list});
-      this.setState({round: 2});
+      this.setState({ curList: list });
+      this.setState({ round: 2 });
       return list;
     }
     else if (round === 3) {
       const snap = await this.props.firebase.db.ref('/companies/').once('value');
-      const list = snap.val().map(e=>({rank: e.round3rank, index: e.index, name: e.companyname}));
-      list.sort(function(a, b){
+      const list = snap.val().map(e => ({ rank: e.round3rank, index: e.index, name: e.companyname }));
+      list.sort(function (a, b) {
         if (a.rank > b.rank) {
           return 1;
         }
-        else{
+        else {
           return -1;
         }
       });
-      this.setState({curList: list.slice(0,5)});
-      this.setState({round: 3});
+      this.setState({ curList: list.slice(0, 5) });
+      this.setState({ round: 3 });
       return list;
     }
     else if (round === 4) {
       const snap = await this.props.firebase.db.ref('/companies/').once('value');
-      const list = snap.val().map(e=>({rank: e.finalrank, index: e.index, name: e.companyname}));
-      list.sort(function(a, b){
+      const list = snap.val().map(e => ({ rank: e.finalrank, index: e.index, name: e.companyname }));
+      list.sort(function (a, b) {
         if (a.rank > b.rank) {
           return 1;
         }
-        else{
+        else {
           return -1;
         }
       });
-      this.setState({curList: list.slice(0,3)});
-      this.setState({round: 4});
+      this.setState({ curList: list.slice(0, 3) });
+      this.setState({ round: 4 });
       return list;
     }
 
@@ -310,8 +310,24 @@ class AdminPage extends Component {
         <h1>1st {this.getRankn(1)}</h1>
         <h1>2nd {this.getRankn(2)}</h1>
         <h1>3rd {this.getRankn(3)}</h1>
-        
-        Round {this.state.round} {this.state.curList.map(e=><div>{e.rank}. {e.name}</div>)}
+        <div id="passlist">
+          Round {this.state.round} {this.state.curList.map(e => <div>{e.rank}. {e.name}</div>)}
+        </div>
+        <button onClick={function () {
+          var copyText = "";
+          for (let i = 0; i < document.getElementById('passlist').children.length; i++) {
+            copyText = copyText + document.getElementById('passlist').children[i].innerHTML;
+            copyText = copyText + "\n";
+          }
+          console.log(copyText);
+          const temp = document.createElement('textarea');
+          temp.value = copyText;
+          document.body.appendChild(temp);
+          temp.select();
+          document.execCommand('Copy');
+          document.body.removeChild(temp);
+          alert('복사되었습니다, 감사합니다.');
+        }}>복사하기</button>
         <button onClick={this.resetInfo}>RESET Users INFO</button>
         <UserList users={users} />
       </div>
