e5ac473f3f0479ad4bbe3fb2d427e6f14dc52bd8
Hojun Lee <quantum0430@gmail.com>
2021-01-25 14:04:33 +0900

    hehe

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+const functions = require("firebase-functions");
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -5,38 +7,3 @@
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
-const functions = require('firebase-functions');
-
-// The Firebase Admin SDK to access Cloud Firestore.
-const admin = require('firebase-admin');
-admin.initializeApp();
-
-exports.addMessage = functions.https.onRequest(async (req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    const writeResult = await admin.firestore().collection('messages').add({original: original});
-    // Send back a message that we've succesfully written the message
-    res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
-
-
-  // Listens for new messages added to /messages/:documentId/original and creates an
-// uppercase version of the message to /messages/:documentId/uppercase
-exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
-.onCreate((snap, context) => {
-  // Grab the current value of what was written to Cloud Firestore.
-  const original = snap.data().original;
-
-  // Access the parameter `{documentId}` with `context.params`
-  functions.logger.log('Uppercasing', context.params.documentId, original);
-
-  const uppercase = original.toUpperCase();
-
-  // You must return a Promise when performing asynchronous tasks inside a Functions such as
-  // writing to Cloud Firestore.
-  // Setting an 'uppercase' field in Cloud Firestore document returns a Promise.
-  return snap.ref.set({uppercase}, {merge: true});
-});
